61e84baacf1366a19939c9db2a27767c618b57e8
anandh <anandh@devop.com>
2021-06-13 04:00:41 +0530

    fridge in paytm

---
+++ src/main/java/paytm/CamundaBpmProcessApplication.java
@@ -0,0 +1,42 @@
+package paytm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.camunda.bpm.application.PostDeploy;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+import org.camunda.bpm.engine.ProcessEngine;
+
+import com.camunda.consulting.util.LicenseHelper;
+
+import static com.camunda.consulting.util.UserGenerator.*;
+import static com.camunda.consulting.util.FilterGenerator.*;
+
+/**
+ * Process Application exposing this application's resources the process engine. 
+ */
+@ProcessApplication
+public class CamundaBpmProcessApplication extends ServletProcessApplication {
+
+  private static final String PROCESS_DEFINITION_KEY = "fridge1";
+
+  /**
+   * In a @PostDeploy Hook you can interact with the process engine and access 
+   * the processes the application has deployed. 
+   */
+  @PostDeploy
+  public void onDeploymentFinished(ProcessEngine processEngine) {
+//    LicenseHelper.setLicense(processEngine);
+//    createDefaultUsers(processEngine);
+//    addGroup(processEngine, "Agent", "Agent", "demo");
+//    addFilterUserAuthorization(processEngine, "demo", FILTER_myTasks, FILTER_groupTasksFilter, FILTER_followUp, FILTER_overdue, FILTER_allTasksFilter);
+
+    // start an initial process instance
+//    Map<String, Object> variables = new HashMap<String, Object>();
+//    variables.put("name", "John");
+//    
+//    processEngine.getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY, variables);
+  }
+
+}
